1de64cc260a09da2bfc91897f6571b759b890968
Кузнецов Максим <maximkuznetsov17250@gmail.com>
2021-12-07 23:32:15 +0300

    fixes #1 update test and close issue

--- src/test/java/ru/netology/web/OrderCardTest.java
+++ src/test/java/ru/netology/web/OrderCardTest.java
@@ -2,9 +2,13 @@ package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
@@ -12,42 +16,36 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class OrderCardTest {
+    String dateMeeting;
+
+    @BeforeEach
+    public void setupDate() {
+        LocalDate date = LocalDate.now();
+        date = date.plusDays(3);
+        int year = date.getYear();
+        int month = date.getMonthValue();
+        int day = date.getDayOfMonth();
+        dateMeeting = day + "." + month + "." + year;
+
+    }
 
     @Test
     public void shouldTest() {
         open("http://localhost:9999/");
 
+        $("[data-test-id=city] input").setValue("Саратов");
 
-        $("[data-test-id=city] [class=input__control]").setValue("Саратов");
-        $("[data-test-id=date] [class=input__control]").setValue("11.12.2021");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
 
-        $("[data-test-id=name] [class=input__control]").setValue("Иванов Иван");
-        $("[data-test-id=phone] [class=input__control]").setValue("+79012347790");
+        $("[data-test-id=date] input").setValue(dateMeeting);
+
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79012347790");
         $(".checkbox__box").click();
 
-        $("button").click();
+        $(".button__text").click();
 
-        $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
-
-
+        $("[data-test-id=\"notification\"]").shouldHave(text("Встреча успешно забронирована на " + dateMeeting), Duration.ofSeconds(15));
     }
 
-    @Test
-    public void shouldTestCollection() {
-        open("http://localhost:9999/");
-
-        ElementsCollection fields = $$("[class=input__control]");
-        fields.get(0).setValue("Волгоград");
-        fields.get(1).setValue("10.12.2021");
-        fields.get(2).setValue("Петров Иван");
-        fields.get(3).setValue("+79012347790");
-
-        $(".checkbox__box").click();
-
-        $("button").click();
-
-        $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
-
-
-    }
 }
